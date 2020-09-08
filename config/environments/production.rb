1cfd6daf7c8170aac2dd72d2891824b616407896
kakame132 <kakame1605@gmail.com>
2020-09-08 14:36:24 +0700

    mailll

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port:                  587,
     user_name:            "kakame1605@gmail.com",
-    password:              "khanhmae12345",
+    password:              "khanhmae123",
     authentication:        "plain"
   }
 end
